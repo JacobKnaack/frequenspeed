67893476b4856847f3e62fcc14ae674748ea2691
Jason Studstill <jason@jasonstudstill.com>
2017-02-25 21:37:17 -0800

    Add modifyLandscape.js (TO Update) / Increase decelleration to 50 easing

--- src/pitchMovement.js
+++ src/pitchMovement.js
@@ -6,18 +6,21 @@ var batchPitches = require('./services/batchPitches.js');
 
 // Game time limit in ms
 var timeLimit = 30000;
-// Number of ms between pitches collected
+
+// Number of ms between collecting pitches
 var msBetweenPitches = 5;
+
 // Timeframe to batch pitches (e.g. collect batch every second) in ms
 var pitchBatchTime = 250;
 
+// Begin Pitch Detection
 pitchDetect(msBetweenPitches, timeLimit);
 batchPitches(normalizePitch, pitchBatchTime, timeLimit);
 
-
+// Update Camera Speed from Pitch
 var cameraSpeedInterval = window.setInterval( function() {
   let camera = document.querySelector('#cameraWrapper');
-  camera.setAttribute('wasd-controls', `acceleration: ${window.currentPitch * 5}; easing: 25`);
+  camera.setAttribute('wasd-controls', `acceleration: ${window.currentPitch * 5}; easing: 50`);
   console.log(camera.components.position.attrValue.z);
   if(camera.components.position.attrValue.z <= -1100) {
     console.log(camera);
@@ -25,8 +28,9 @@ var cameraSpeedInterval = window.setInterval( function() {
     camera.setAttribute('wasd-controls', 'acceleration: 0');
     window.clearInterval(cameraSpeedInterval);
   }
-}, 250);
+}, pitchBatchTime);
 
+// Stop updating speed when time runs out
 window.setTimeout(function() {
   window.clearInterval(cameraSpeedInterval);
-}, 30000);
+}, timeLimit);
---
+++ src/services/modifyLandscape.js
@@ -0,0 +1,1 @@
+// TODO - Add logic to dynamically build parts of the scene
