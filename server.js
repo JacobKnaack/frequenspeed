0928d04ad013fba6687ba3af428beb6737931e5e
Jacob Knaack <jacobknck@gmail.com>
2017-02-26 15:28:02 -0800

    added packages to fix heroku build

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const port = process.env.PORT || 8080
 
-app.use(express.static(`${__dirname}/build`))
+app.use(morgan('dev'))
+app.use(express.static(__dirname +/build))
 
 app.listen(port, function(){
   console.log('server up on', port);
