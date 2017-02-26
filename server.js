b1015124d73fcadd7f4faf4dc8ddbfd813c7566e
Jacob Knaack <jacobknck@gmail.com>
2017-02-26 15:30:55 -0800

    fixed server typo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require('morgan')
 const port = process.env.PORT || 8080
 
 app.use(morgan('dev'))
-app.use(express.static(__dirname +/build))
+app.use(express.static(__dirname + '/build'))
 
 app.listen(port, function(){
   console.log('server up on', port);
