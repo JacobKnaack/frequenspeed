6b697ed66401c1d92aa7822e1edd7a7252644d57
Jacob Knaack <jacobknck@gmail.com>
2017-02-25 18:45:19 -0800

    working on stopping at track ending

--- src/pitchMovement.js
+++ src/pitchMovement.js
@@ -2,8 +2,14 @@
 
 var cameraSpeedInterval = window.setInterval( function() {
   let camera = document.querySelector('#cameraWrapper')
-  camera.setAttribute('wasd-controls', `acceleration: ${window.currentPitch * 5}; easing: 30`)
-  console.log(camera);
+  camera.setAttribute('wasd-controls', `acceleration: ${window.currentPitch * 5}; easing: 25`)
+  console.log(camera.components.position.attrValue.z);
+  if(camera.components.position.attrValue.z <= -1100) {
+    console.log(camera);
+    camera.setAttribute('wasd-controls', 'enabled: false')
+    camera.setAttribute('wasd-controls', 'acceleration: 0')
+    window.clearInterval(cameraSpeedInterval)
+  }
 }, 250)
 
 window.setTimeout(function() {
